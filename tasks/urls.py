18048825ae9af658b2261157bf5189b5fef7b846
Mayank1811605 <mayankgupta1809@gmail.com>
2025-10-02 23:23:47 +0530

    top level endpoind added

--- tasks/urls.py
+++ tasks/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import tasks.views as views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.tasks),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+import json
+
+task_list = [
+    {
+    "id": 1,
+    "title": "Create a new project",
+    "description": "Create a new project using Magic",
+    "completed": False
+  }
+]
 
 # Create your views here.
+@api_view(["GET", "POST"])
+def tasks(request):
+    
+    if request.method == "GET":
+        return Response({"tasks": task_list}, status=status.HTTP_200_OK)
+    
+    if request.method == "POST":
+        print(request.data)
+        task_list.append(request.data)
+        return Response({"tasks": task_list}, status=status.HTTP_201_CREATED)
